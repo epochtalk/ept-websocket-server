722185b6d4c852adad2c5e3557864dfdc31733aa
Bronson Oka <boka@slickage.com>
2016-03-03 17:36:57 -1000

    create a worker for every CPU

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 require('dotenv').load();
+var os = require('os');
 var SocketCluster = require('socketcluster').SocketCluster;
 
 var socketCluster = new SocketCluster({
-  workers: 3,
+  workers: os.cpus().length,
   brokers: 3,
   port: process.env.SOCKETCLUSTER_PORT,
   appName: 'myapp',
