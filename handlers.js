77bc8473f3d2151e04ffb11461fb46001c3434dd
Bronson Oka <boka@slickage.com>
2016-05-05 19:26:18 -1000

    parse channel name in postprocessSubscribe

--- handlers.js
+++ handlers.js
@@ -1,6 +1,9 @@
+var path = require('path');
+var helper = require(path.join(__dirname, 'helper'));
 var handlers = module.exports = {};
 
-handlers.postprocessSubscribe = function(data) {
-  console.log('SUBSCRIBED:', data.type, data.id);
+handlers.postprocessSubscribe = function(channel) {
+  channel = helper.parseChannel(channel);
+  console.log('SUBSCRIBED:', channel.type, channel.id);
   // postprocess subscriptions
 };
