2a082cc6c0ac65ff293fc73c66b70877bc02861f
Bronson Oka <boka@slickage.com>
2016-05-05 19:02:59 -1000

    use JSON.parse to extract object from json string
    on middleware subscribe handler, get the channel object from json string

--- middleware.js
+++ middleware.js
@@ -4,29 +4,41 @@ var db = require(path.join(__dirname, 'db'));
 
 var middleware = module.exports = {};
 middleware.subscribe = function(req, next) {
+  var channel = parseChannel(req.channel);
   var token = req.socket.getAuthToken();
-  if (token) {
-    db.users.find(token.userId).then(function(dbUser) {
-      // check for user channel
-      if (req.channel.type === 'user' && req.channel.id === dbUser.id) {
-        next();
-      }
-      // check for role channel
-      else if (_.some(dbUser.roles, roleChannelLookup(req.channel))) {
-        next();
-      }
-      else {
-        next('MIDDLEWARE_SUBSCRIBE: Unauthorized channel ' + req.channel);
-      }
-    })
-    .catch(function(err) {
-      next('MIDDLEWARE_SUBSCRIBE: ' + err);
-    });
+
+  if (channel) {
+    if (token) {
+      db.users.find(token.userId).then(function(dbUser) {
+        // check for user channel
+        if (channel['type'] === 'user' && channel['id'] === dbUser.id) {
+          next();
+        }
+        // check for role channel
+        else if (channel['type'] === 'role' && _.some(dbUser.roles, roleChannelLookup(channel['id']))) {
+          next();
+        }
+        else {
+          next('MIDDLEWARE_SUBSCRIBE: Unauthorized channel ' + req.channel);
+        }
+      })
+      .catch(function(err) {
+        next('MIDDLEWARE_SUBSCRIBE: ' + err);
+      });
+    }
+    else {
+      next('MIDDLEWARE_SUBSCRIBE: Missing token.');
+    }
   }
   else {
-    next('MIDDLEWARE_SUBSCRIBE: Missing token.');
+    next('MIDDLEWARE_SUBSCRIBE: Invalid channel format ', req.channel);
   }
 };
+
+function parseChannel(reqChannel) {
+  try { return JSON.parse(reqChannel); }
+  catch(err){ return undefined; }
+}
 
 function roleChannelLookup(channelRole) {
   return function(role) {
