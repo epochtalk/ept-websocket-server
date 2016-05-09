f3c9329e50d051a7d573ab10c04cb16363e4370e
Bronson Oka <boka@slickage.com>
2016-05-09 13:31:42 -1000

    update subscription handler to mark online users

--- handlers.js
+++ handlers.js
@@ -1,9 +1,15 @@
 var path = require('path');
 var helper = require(path.join(__dirname, 'helper'));
+var onlineUsers = require(path.join(__dirname, 'online'));
 var handlers = module.exports = {};
 
-handlers.postprocessSubscribe = function(channel) {
+handlers.postprocessSubscribe = function(options) {
+  var channel = options.channel;
   channel = helper.parseChannel(channel);
   console.log('SUBSCRIBED:', channel.type, channel.id);
   // postprocess subscriptions
+  if (channel.type === 'user') {
+    onlineUsers.add({ userId: channel.id, socketId: options.socketId });
+    console.log('onlineusers', onlineUsers.show(), onlineUsers.get());
+  }
 };
