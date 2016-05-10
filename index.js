20707cb9e8e63a99695b59f089b3dc55b93f583c
Bronson Oka <boka@slickage.com>
2016-05-09 18:30:18 -1000

    redis: clear store and log config on startup

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ var SocketCluster = require('socketcluster').SocketCluster;
 
 var config = require(path.join(__dirname, 'config'));
 
+var onlineUsers = require(path.join(__dirname, 'online'));
 var db = require(path.join(__dirname, 'db'));
 db.users.testConnection()
+.then(onlineUsers.logOptions)
+.then(onlineUsers.clear)
 .then(function() {
   return new Promise(function(resolve, reject) {
     var socketCluster = new SocketCluster({
