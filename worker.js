7ac84b3432ef945e111e3738a785b45b35ea00e7
Bronson Oka <boka@slickage.com>
2016-05-11 15:23:31 -1000

    don't use handlers in worker
    no longer necessary; all postprocessing is done in broker.js

--- worker.js
+++ worker.js
@@ -2,7 +2,6 @@ var path = require('path');
 var config = require(path.join(__dirname, 'config'));
 
 var middleware = require(path.join(__dirname, 'middleware'));
-var handlers = require(path.join(__dirname, 'handlers'));
 
 module.exports.run = function(worker) {
   var scServer = worker.scServer;
@@ -11,9 +10,6 @@ module.exports.run = function(worker) {
   scServer.addMiddleware(scServer.MIDDLEWARE_SUBSCRIBE, middleware.subscribe);
 
   scServer.on('connection', function(socket) {
-    socket.on('subscribe', function(channel) {
-      handlers.postprocessSubscribe({ socketId: socket.id, channel: channel});
-    });
     console.log('CONNECTION: connected to', process.pid);
     socket.on('notify', function(options) {
       // don't allow API key to be sent to client
@@ -28,7 +24,6 @@ module.exports.run = function(worker) {
       socket.kickOut();
     });
     socket.on('disconnect', function() {
-      handlers.postprocessDisconnect(socket);
       console.log('DISCONNECT:', process.pid);
     });
     socket.on('error', function(error) {
