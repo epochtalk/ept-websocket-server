124dd9b94795af603c38c8e7d5b397a68721d902
Bronson Oka <boka@slickage.com>
2016-05-17 13:01:19 -1000

    publish redis channel messages on (un)subscribe
    this will be used to send out subscription messages to all users when
    the online users list updates

--- broker.js
+++ broker.js
@@ -7,18 +7,20 @@ module.exports.run = function(broker) {
   broker.on('subscribe', function(channel) {
     var parsedChannel = helper.parseJSON(channel);
     if (parsedChannel.type === 'user') {
+      console.log('BROKER SUBSCRIBE', parsedChannel.id);
       onlineUsers.add({ userId: parsedChannel.id, brokerId: broker.id });
       onlineUsers.show().then(function(online) {
-        console.log('BROKER SUBSCRIBE', parsedChannel.id);
+        onlineUsers.publish(config.redisChannels.onlineUsersChannel, JSON.stringify(online));
       });
     }
   });
   broker.on('unsubscribe', function(channel) {
     var parsedChannel = helper.parseJSON(channel);
     if (parsedChannel.type === 'user') {
+      console.log('BROKER UNSUBSCRIBE', parsedChannel.id);
       onlineUsers.remove({ userId: parsedChannel.id, brokerId: broker.id });
       onlineUsers.show().then(function(online) {
-        console.log('BROKER UNSUBSCRIBE', parsedChannel.id);
+        broker.publish(JSON.stringify({ type: 'public' }), { onlineUsers: online });
       });
     }
   });
