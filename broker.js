9fc860af7975eebede8017ff8b84af394db94b1e
Bronson Oka <boka@slickage.com>
2016-05-18 14:21:36 -1000

    use online.get in broker.js

--- broker.js
+++ broker.js
@@ -15,7 +15,7 @@ module.exports.run = function(broker) {
     if (parsedChannel.type === 'user') {
       console.log('BROKER SUBSCRIBE', parsedChannel.id);
       onlineUsers.add({ userId: parsedChannel.id, brokerId: broker.id });
-      onlineUsers.show().then(function(online) {
+      onlineUsers.get().then(function(online) {
         onlineUsers.publish(config.redisChannels.onlineUsersChannel, JSON.stringify(online));
       });
     }
@@ -25,7 +25,7 @@ module.exports.run = function(broker) {
     if (parsedChannel.type === 'user') {
       console.log('BROKER UNSUBSCRIBE', parsedChannel.id);
       onlineUsers.remove({ userId: parsedChannel.id, brokerId: broker.id });
-      onlineUsers.show().then(function(online) {
+      onlineUsers.get().then(function(online) {
         broker.publish(JSON.stringify({ type: 'public' }), { onlineUsers: online });
       });
     }
